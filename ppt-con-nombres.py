97a497d28b6c4c343fd6ce091b687c7affe8df62
Pachi <mpazaguilarm@gmail.com>
2025-02-27 11:10:24 -0300

    Piedra, papel o tijera con nonombres

---
+++ ppt-con-nombres.py
@@ -0,0 +1,16 @@
+
+
+nombre1 = input("¿Cómo se llamará el jugador 1?: ")
+nombre2 = input("¿Cómo se llamará el jugador 2?: ")
+
+jugador1 = input("Hola, " + nombre1 + ". ¿Qué eliges? ¿Piedra, papel o tijera?: ")
+jugador2 = input("Hola, " + nombre2 + ". ¿Qué eliges? ¿Piedra, papel o tijera?: ")
+
+
+if jugador1 == jugador2:
+    print("Ha sido un empate")
+elif (jugador1 == "piedra" and jugador2 == "tijera") or (jugador1 == "papel" and jugador2 == "piedra") or (jugador1 == "tijera" and jugador2 == "papel"):
+    print("Ha ganado: ", nombre1)
+    #sino puedo escribirlas como condicion1, condicion 2, y condicion 3
+else:
+    print("Ha ganado: ", nombre2)
